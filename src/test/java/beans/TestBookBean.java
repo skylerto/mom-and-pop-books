38db20acbd6d0ff8bc47c8d2a3161b1e7b8c8bfb
Skyler Layne <skylerclayne@gmail.com>
2016-02-08 15:23:58 -0500

    Okay -p
    missed a few files when using -p flag, these are those files.
    
    My bad!

---
+++ src/test/java/beans/TestBookBean.java
@@ -0,0 +1,119 @@
+package beans;
+
+import beans.BookBean;
+import junit.framework.TestCase;
+
+public class TestBookBean extends TestCase {
+
+	BookBean b;
+
+	public void testBookBean() {
+		// Null to start
+		assertNull(b);
+
+		// Create with empty arguments
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+		BookBean c = new BookBean(bid, title, price, category);
+		//assertEquals(b, c);
+
+		b = null;
+		assertNull(b);
+	}
+
+	public void testGetBid() {
+
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+
+		assertEquals("", b.getBid());
+	}
+
+	public void testSetBid() {
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+		b.setBid("BID2");
+		assertEquals("BID2", b.getBid());
+	}
+
+	public void testGetTitle() {
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+
+		assertEquals("", b.getTitle());
+	}
+
+	public void testSetTitle() {
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+		b.setTitle("A title");
+		assertEquals("A title", b.getTitle());
+	}
+
+	public void testGetPrice() {
+		String bid = "";
+		String title = "";
+		double price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+
+		assertEquals(0.0, b.getPrice());
+	}
+
+	public void testSetPrice() {
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+		b.setPrice(36.50);
+		assertEquals(36.50, b.getPrice());
+	}
+
+	public void testGetCategory() {
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+
+		assertEquals("", b.getCategory());
+	}
+
+	public void testSetCategory() {
+		String bid = "";
+		String title = "";
+		int price = 0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+		b.setCategory("A Category");
+		assertEquals("A Category", b.getCategory());
+	}
+
+	public void testToString() {
+		String bid = "";
+		String title = "";
+		double price = 0.0;
+		String category = "";
+		b = new BookBean(bid, title, price, category);
+		
+		String res = "Book: [" + "" + ", " + "" + ", $" + 0.0 + ", " + "" + "]";
+		assertEquals(res, b.toString());
+	}
+
+}
