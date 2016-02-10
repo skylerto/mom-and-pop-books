ac86a228c981d47a055c2eb4da003824f2e208f9
Skyler Layne <skylerclayne@gmail.com>
2016-02-09 19:21:01 -0500

    CartBean and Tests
    Add cartbean with methods.
    
    Unit tested with 100% coverage.

--- src/test/java/beans/TestCartBean.java
+++ src/test/java/beans/TestCartBean.java
@@ -1,40 +1,77 @@
 package beans;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import junit.framework.TestCase;
 
-public class TestCartBean {
+public class TestCartBean extends TestCase {
 
-	private static CartBean cart;
+	private CartBean cart;
+	private BookBean b1;
+	private BookBean b2;
 
-	@Before
-	public void setUp() throws Exception {
+	protected void setUp() throws Exception {
+		super.setUp();
 		cart = new CartBean();
+		b1 = new BookBean("0201633612", "Design Patterns: Elements of Reusable Object-Oriented Software", 55.09,
+				"Programming");
+		b2 = new BookBean("0978739213", "Release It!: Design and Deploy Production-Rea​dy Software", 40.11,
+				"Programming");
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 
-	@Test
-	public void testAddToCart() {
-		// A Book
+	public void testCartSize() {
+
+		assertEquals(0, cart.size());
 
 		// Add the Book
+		cart.add(b1);
 
 		// Check the size
+		assertEquals(1, cart.size());
 
 	}
 
-	@Test
+	public void testAddToCart() {
+		// A Book
+		assertTrue(cart.add(b1));
+
+		for (int i = 0; i < 50; i++) {
+			cart.add(b1);
+		}
+
+		assertFalse(cart.add(b1));
+
+	}
+
 	public void testGetCart() {
 		// Add Some Books
+		assertEquals(new ArrayList<>(), cart.get());
 
-		// Check if the books are there
+		List<BookBean> books = new ArrayList<>();
+		books.add(b1);
+		cart.add(b1);
+		assertEquals(books, cart.get());
 
+
+		books.add(b2);
+		cart.add(b2);
+		assertEquals(books, cart.get());
+
+		assertEquals(2, cart.size());
+
+	}
+
+	public void testGetBookByIdCart() {
+		cart.add(b1);
+		cart.add(b2);
+
+		assertNull(cart.getBook("This is fake"));
+		assertEquals(b1, cart.getBook(b1.getBid()));
 	}
 
 }
