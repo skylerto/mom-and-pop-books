ebbbc38a8e43b865975083161e9b8d8b1e1a345f
Siraj Rauff <sirajrauff@gmail.com>
2016-04-01 00:02:56 -0400

    Added Listener
    - Deleted Filter
    - New cart now created when session is created

---
+++ src/main/java/listeners/StartListener.java
@@ -0,0 +1,34 @@
+package listeners;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import models.Cart;
+
+/**
+ * Application Lifecycle Listener implementation class MaxPrinciple
+ *
+ */
+@WebListener
+public class StartListener implements HttpSessionListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public StartListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		arg0.getSession().setAttribute("cart", new Cart());
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
