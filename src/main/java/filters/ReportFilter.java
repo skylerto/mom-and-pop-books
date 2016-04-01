37b64f7c830a98d0acdedd86d46e96db713b0bb3
Siraj Rauff <sirajrauff@gmail.com>
2016-04-01 02:20:37 -0400

    Allow only Admin access to reports

---
+++ src/main/java/filters/ReportFilter.java
@@ -0,0 +1,70 @@
+package filters;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.VisitEventBean;
+import dao.BookDataAccessObject;
+import models.Books;
+
+/**
+ * Servlet Filter implementation class ReportFilter
+ */
+@WebFilter(
+		dispatcherTypes = { DispatcherType.REQUEST },
+		urlPatterns = { "/Reports/*", "/Reports" })
+public class ReportFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public ReportFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession currSess = req.getSession();
+
+		boolean admin = currSess.getAttribute("admin") == null ? false : (boolean) currSess.getAttribute("admin");
+		if (!admin) {
+			res.sendRedirect(request.getServletContext().getContextPath() + "/");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
